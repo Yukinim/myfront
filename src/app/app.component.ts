48639d6f136ce7b723d954a4006493792e10e158
Yukinim <yukinim86@gmail.com>
2018-05-06 21:24:41 +0900

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
 import APP_CONFIG from './app.config';
 import { Node, Link } from './d3';
+import { HttpClient } from '@angular/common/http';
+
+interface MyData {
+  data: Keyword[];
+}
+
+interface Keyword {
+  KEYWORD: string;
+  CNT: number;
+  relKeywords: RelKeyword[];
+}
+
+interface RelKeyword {
+  KEYWORD: string;
+  COUNT: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -11,25 +27,71 @@ import { Node, Link } from './d3';
 export class AppComponent {
   nodes: Node[] = [];
   links: Link[] = [];
-
-  constructor() {
-    const N = APP_CONFIG.N,
-          getIndex = number => number - 1;
-
+  keywordList: Keyword[] = [];
+  
+  constructor(private httpClient:HttpClient) {
+    const N = APP_CONFIG.N;
     /** constructing the nodes array */
-    for (let i = 1; i <= N; i++) {
-      this.nodes.push(new Node(i, 'KEYWORD'+i, i));
-    }
+    this.httpClient
+    .get<MyData>("/api/v1/mykeywords")
+    .subscribe(
 
-    for (let i = 1; i <= N; i++) {
-      for (let m = 2; i * m <= N; m++) {
-        /** increasing connections toll on connecting nodes */
-        this.nodes[getIndex(i)].linkCount++;
-        this.nodes[getIndex(i * m)].linkCount++;
+      (ret:any) => {
+        this.keywordList = ret.data;
+        APP_CONFIG.N = this.keywordList.length;
 
-        /** connecting the nodes before starting the simulation */
-        this.links.push(new Link(i, i * m));
+        // for (let i=0; i<this.keywordList.length ; i++) {
+        //   this.nodes.push(new Node(i, this.keywordList[i].KEYWORD, this.keywordList[i].CNT));
+        // }
+        // alert(JSON.stringify(this.keywordList));
+        // for (let i = 0; i < N-1; i++) {
+        //   for (let m = i+1; m < N; m++) {
+        //     for(let k = 0; k < this.keywordList[i].relKeywords.length; k ++ ){
+        //       if(this.keywordList[i].relKeywords[k].KEYWORD == this.keywordList[m].KEYWORD){
+        //         this.nodes[i].linkCount++;
+        //         this.nodes[m].linkCount++;
+        //         this.links.push(new Link(i, m));
+        //         break;
+        //       }
+        //     } 
+        //   }
+        // }
+       }) 
+       
+      // for (let i = 1; i <= N; i++) {
+      //   this.nodes.push(new Node(i, 'KEYWORD'+i, i));
+      // }
+  
+      // for (let i = 1; i <= N; i++) {
+      //   for (let m = 2; i * m <= N; m++) {
+      //     /** increasing connections toll on connecting nodes */
+      //     this.nodes[i-1].linkCount++;
+      //     this.nodes[(i * m)-1].linkCount++;
+  
+      //     /** connecting the nodes before starting the simulation */
+      //     this.links.push(new Link(i, i * m));
+      //   }
+      // }
+
+      for (let i=0; i<this.keywordList.length ; i++) {
+        this.nodes.push(new Node(i, this.keywordList[i].KEYWORD, this.keywordList[i].CNT));
       }
-    }
+      alert(JSON.stringify(this.keywordList));
+      for (let i = 0; i < N-1; i++) {
+        for (let m = i+1; m < N; m++) {
+          for(let k = 0; k < this.keywordList[i].relKeywords.length; k ++ ){
+            if(this.keywordList[i].relKeywords[k].KEYWORD == this.keywordList[m].KEYWORD){
+              this.nodes[i].linkCount++;
+              this.nodes[m].linkCount++;
+              this.links.push(new Link(i, m));
+              break;
+            }
+          } 
+        }
+      }
+  }
+
+  myEvent(){
+
   }
 }
